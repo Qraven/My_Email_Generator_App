e031b1b9bd9dc755317fc079aa9a6f3d6d0cbb31
Qraven <32140063+Qraven@users.noreply.github.com>
2019-02-06 18:52:25 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+/*
+This is a simple app made in Java by Qraven. It can be used to generate email addresses and creating random passwords. Hope you enjoy!
+*/
 public class Main {
 
     public static void main(String[] args){
 
-        Email em1 = new Email("Jimmy", "Bronco");
-        em1.createDepartment();
-        em1.createEmail();
-        em1.passwordRandom(100);
+        Email em1 = new Email("Jimmy", "Bronco"); //Creating Email
+        em1.createDepartment(); //Asking and creating department code
+        em1.createEmail(); //Creating email from a given data
+        em1.passwordRandom(10); //Creating a random password
 
     }
 }
